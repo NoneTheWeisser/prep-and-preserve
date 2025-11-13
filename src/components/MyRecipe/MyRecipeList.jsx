d3992196ec91ed8792048b7c02764e9a4c2bce59
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-11-13 13:41:57 -0600

    going to go with a basic toggle for now

--- src/components/MyRecipe/MyRecipeList.jsx
+++ src/components/MyRecipe/MyRecipeList.jsx
@@ -103,7 +103,7 @@ export default function MyRecipeList() {
               p: 2,
               mb: 3,
               borderBottom: "1px solid #e0e0e0",
-              backgroundColor: "#eae9dbff",
+              // backgroundColor: "#eae9dbff",
               borderRadius: 2,
             }}
           >
